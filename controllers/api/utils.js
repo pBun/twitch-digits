4d15c5257ce2db6511f7a650fbf8ce8b0e1cdcae
Boersma, Dustin <dustin.boersma@gmail.com>
2017-10-08 19:17:51 -0700

    removed api code

--- controllers/api/utils.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    jsonRes: (res, data, error) => {
-        res.json({
-            status: !error ? 'success' : 'error',
-            message: !error ? 'ok' : error,
-            data: data
-        });
-    }
-};
--- models/twitch/index.js
+++
@@ -1,9 +0,0 @@
-var games = require('./games');
-var streams = require('./streams');
-var summary = require('./summary');
-
-module.exports = {
-    streams: streams,
-    games: games,
-    summary: summary
-};
