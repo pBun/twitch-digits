529fc83320418808d7734f4e0e8ea0c85687b0ee
Dustin Boersma <dustin.boersma@gmail.com>
2017-12-31 18:56:47 -0800

    removed redux for now

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import store from './reducers';
 import App from './components/TwitchDigits/index.jsx';
 
